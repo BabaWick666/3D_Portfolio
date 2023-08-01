ae1872028f46fd572d80783d404aa81fd7aeb781
BabaWick666 <rishi.suyash01@gmail.com>
2023-08-01 08:51:21 +0530

    minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+import { ComputersCanvas } from "./canvas/Computers";
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
@@ -21,6 +22,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
+      <ComputersCanvas />
     </section>
   );
 };
